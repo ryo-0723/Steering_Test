6530feb66e8a9a804e0664c6e6c2907dbceefd54
ryo-0723 <98096559+ryo-0723@users.noreply.github.com>
2023-04-24 01:45:15 +0900

    回転の移動ができてない

--- steering/steering/Object.cpp
+++ steering/steering/Object.cpp
@@ -20,7 +20,7 @@ bool  fliped_jadge(double before, double after) {
 }
 
 Object& Object::draw(const Color& Framecolor=Palette::White) {
-	Robot.setCenter(400,300).moveBy(Robot_Vec.x,-Robot_Vec.y).rotated(Robot_Vec.z).drawFrame(0, 1, Framecolor);
+	Robot.setCenter(400,300).moveBy(Robot_Vec.x,-Robot_Vec.y).rotatedAt(Robot.center(), Robot_Vec.z).drawFrame(0, 1, Framecolor);
 	/*ロボットの外枠*/
 	//LF_frame.set( Robot.pos,size / 3 ).drawFrame(0, 1, Framecolor);
 	///*ロボットの左上*/
@@ -31,13 +31,13 @@ Object& Object::draw(const Color& Framecolor=Palette::White) {
 	//RB_frame.set(Robot.br() - size / 3, size / 3).drawFrame(0, 1, Framecolor);
 	///*右下*/
 
-	Rectframe[0].set(Robot.pos, size / 3).drawFrame(0, 1, Framecolor);
+	Rectframe[0].set(Robot.pos, size / 3).rotatedAt(Robot.center(), Robot_Vec.z).drawFrame(0, 1, Framecolor);
 	/*ロボットの左上*/
-	Rectframe[1].set(Robot.tr().x - size.x / 3, Robot.pos.y, size / 3).drawFrame(0, 1, Framecolor);
+	Rectframe[1].set(Robot.tr().x - size.x / 3, Robot.pos.y, size / 3).rotatedAt(Robot.center(), Robot_Vec.z).drawFrame(0, 1, Framecolor);
 	/*右上*/
-	Rectframe[2].set(Robot.br() - size / 3, size / 3).drawFrame(0, 1, Framecolor);
+	Rectframe[2].set(Robot.br() - size / 3, size / 3).rotatedAt(Robot.center(), Robot_Vec.z).drawFrame(0, 1, Framecolor);
 	/*右下*/
-	Rectframe[3].set(Robot.bl().x, Robot.bl().y - size.y / 3, size / 3).drawFrame(0, 1, Framecolor);
+	Rectframe[3].set(Robot.bl().x, Robot.bl().y - size.y / 3, size / 3).rotatedAt(Robot.center(), Robot_Vec.z).drawFrame(0, 1, Framecolor);
 	/*左下*/
 	return *this;
 }
@@ -71,10 +71,11 @@ Object& Object::SetPower(std::array<double,4>length, std::array<double,4>angle) 
 	for (int i = 0; i < 4; i++) {
 		Vector[i] = { length[i] * sin(ToRadians(angle[i])) ,length[i] * cos(ToRadians(angle[i])) };
 		RectF{ 0,0,Rectframe[i].w / 3.1,Rectframe[i].h / 1.5 }.setCenter(Rectframe[i].center()).rotated(ToRadians(angle[i]))
+			.rotatedAt(Robot.center(), Robot_Vec.z)
 			.draw(Palette::Red, Palette::Red, Palette::White, Palette::White);
-		Shape2D::Arrow(Rectframe[i].center()
-			, { Rectframe[i].center().x + Vector[i].x
-			, Rectframe[i].center().y - Vector[i].y }
+		Shape2D::Arrow(Rectframe[i].center().rotatedAt(Robot.center(), Robot_Vec.z)
+			, { Rectframe[i].center().rotatedAt(Robot.center(), Robot_Vec.z).x + Vector[i].x
+			, Rectframe[i].center().rotatedAt(Robot.center(), Robot_Vec.z).y - Vector[i].y }
 			, 6
 			, Vec2{ 8,8 }).draw();
 
@@ -82,16 +83,20 @@ Object& Object::SetPower(std::array<double,4>length, std::array<double,4>angle) 
 			time[i] = Time::GetMillisec();
 		}
 		if (Time::GetMillisec() - time[i] < 200) {
-			Rectframe[i].drawFrame(0, 3, Palette::Orange);
+			Rectframe[i].rotatedAt(Robot.center(), Robot_Vec.z).drawFrame(0, 3, Palette::Orange);
 		}
 		old_angle[i] = angle[i];
-	}
-	for (int i = 0; i < 4;i++) {
 		/*回転ベクトル以外のベクトルを求める*/
-		Robot_Vec.x += Vector[i].x/100;
-		Robot_Vec.y += Vector[i].y/100;
-		Robot_Vec.z += Vec2{ 0,Vector[i].y }.rotate((i * -0.5 - 0.25) * Math::Pi).x;
+		Robot_Vec.x += Vector[i].rotatedAt(Robot.center(), Robot_Vec.z).x/100;
+		Robot_Vec.y += Vector[i].rotatedAt(Robot.center(), Robot_Vec.z).y/100;
+		/*出来てない*/
+		Robot_Vec.z += (/*ｘ軸*/length[i] * sin(ToRadians(angle[i]) + (i * -0.5 - 0.75) * Math::Pi) - /*ｙ軸*/length[i] * cos(ToRadians(angle[i]) + (i * -0.5 - 0.25) * Math::Pi))/1000;
+		//Robot_Vec.z += Vec2{ Vector[i].x,Vector[i].y }.rotatedAt(Robot.center(), Robot_Vec.z).rotate((i * -0.5 + 0.25) * Math::Pi).x / 10000;
+		Print << U"::";
+		Print << length[i] * sin(ToRadians(angle[i]) + (i * -0.5 - 0.75) * Math::Pi);
+		Print << length[i] * cos(ToRadians(angle[i]) + (i * -0.5 - 0.25) * Math::Pi);
 	}
+	
 	return *this;
 }
 //Object& Object::SetLF(double length,double angle) {
