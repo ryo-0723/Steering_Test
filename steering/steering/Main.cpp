d080854225def3ce53701be6247f3db04bcb4f66
ryo-0723 <98096559+ryo-0723@users.noreply.github.com>
2023-04-24 18:15:43 +0900

    ver1.0

--- steering/steering/Main.cpp
+++ steering/steering/Main.cpp
@@ -17,23 +17,28 @@ void Main()
 	while (System::Update())
 	{
 		ClearPrint();
-		if (const auto gamepad=Gamepad(1)) {
+		
+		if (const auto gamepad=Gamepad(0)) {
 			/*ps5だったらGamepad(0)だよ*/
+			/*ps5だったらGamepad(1)だよ*/
 			//const auto& info = gamepad.getInfo();
 			LeftAxis.x = (int)(gamepad.axes.at(0)*255);
 			LeftAxis.y = -(int)(gamepad.axes.at(1)*255);
-			InputTurn = (int)(gamepad.axes.at(3)*255);
+			InputTurn = (int)(gamepad.axes.at(2)*255);
 			/*ps5だったらat(2)だよ*/
+			/*ps4だったらat(3)だよ*/
+
 			Print << LeftAxis <<U" "<< InputTurn;
 		}
+
+		//LeftAxis.y = KeyW.pressed()*40 - KeyS.pressed()*40;
+		//LeftAxis.x = KeyD.pressed()*40 - KeyA.pressed()*40;
+		//InputTurn = KeyRight.pressed()*40 - KeyLeft.pressed()*40;
+
 		Print << Scene::DeltaTime()*1000;
 
 		steer.Update(LeftAxis, InputTurn, 0);
-		//robot.SetRF(steer.GetRF_l(), steer.GetRF_a())
-		//	 .SetRB(steer.GetRB_l(), steer.GetRB_a())
-		//	 .SetLF(steer.GetLF_l(), steer.GetLF_a())
-		//	 .SetLB(steer.GetLB_l(), steer.GetLB_a())
-		//	 .draw(color);
+
 		std::array<double, 4>length = { steer.GetLF_l() ,steer.GetRF_l() ,steer.GetRB_l() ,steer.GetLB_l() };
 		std::array<double, 4>angle  = { steer.GetLF_a() ,steer.GetRF_a() ,steer.GetRB_a() ,steer.GetLB_a() };
 		robot.SetPower(length ,angle ).draw(color);
--- steering/steering/Object.cpp
+++ steering/steering/Object.cpp
@@ -22,14 +22,7 @@ bool  fliped_jadge(double before, double after) {
 Object& Object::draw(const Color& Framecolor=Palette::White) {
 	Robot.setCenter(400,300).moveBy(Robot_Vec.x,-Robot_Vec.y).rotatedAt(Robot.center(), Robot_Vec.z).drawFrame(0, 1, Framecolor);
 	/*ロボットの外枠*/
-	//LF_frame.set( Robot.pos,size / 3 ).drawFrame(0, 1, Framecolor);
-	///*ロボットの左上*/
-	//RF_frame.set(Robot.tr().x - size.x / 3, Robot.pos.y, size / 3).drawFrame(0, 1, Framecolor);
-	///*右上*/
-	//LB_frame.set(Robot.bl().x, Robot.bl().y - size.y / 3, size / 3).drawFrame(0, 1, Framecolor);
-	///*左下*/
-	//RB_frame.set(Robot.br() - size / 3, size / 3).drawFrame(0, 1, Framecolor);
-	///*右下*/
+	Line{Robot.tl().rotatedAt(Robot.center(), Robot_Vec.z),Robot.tr().rotatedAt(Robot.center(), Robot_Vec.z) }.draw(3, Palette::Red);
 
 	Rectframe[0].set(Robot.pos, size / 3).rotatedAt(Robot.center(), Robot_Vec.z).drawFrame(0, 1, Framecolor);
 	/*ロボットの左上*/
@@ -41,29 +34,6 @@ Object& Object::draw(const Color& Framecolor=Palette::White) {
 	/*左下*/
 	return *this;
 }
-//
-//Object& Object::SetLF(Vec2 r) {
-//	Circle{ LF_frame.center().x + r.x, LF_frame.center().y - r.y ,7 }.draw();
-//	//Shape2D::Arrow(LF_frame.center(), { LF_frame.center().x + r.x, LF_frame.center().y - r.y }, 5, Vec2{ 5,5 }).draw();
-//	return *this;
-//}
-//Object& Object::SetRF(Vec2 r) {
-//	Circle{ RF_frame.center().x + r.x, RF_frame.center().y - r.y ,7 }.draw();
-//	//Shape2D::Arrow(RF_frame.center(), { RF_frame.center().x + r.x, RF_frame.center().y - r.y }, 5, Vec2{ 5,5 }).draw();
-//	return *this;
-//}
-//Object& Object::SetRB(Vec2 r) {
-//	Circle{ RB_frame.center().x + r.x, RB_frame.center().y - r.y ,7 }.draw();
-//	//Shape2D::Arrow(RB_frame.center(), { RB_frame.center().x + r.x, RB_frame.center().y - r.y }, 5, Vec2{ 5,5 }).draw();
-//	return *this;
-//}
-//Object& Object::SetLB(Vec2 r) {
-//	Circle{ LB_frame.center().x + r.x, LB_frame.center().y - r.y ,7 }.draw();
-//	//Shape2D::Arrow(LB_frame.center(), { LB_frame.center().x + r.x, LB_frame.center().y - r.y }, 5, Vec2{ 5,5 }).draw();
-//	return *this;
-//}
-
-
 
 /*赤になっているほうが0°のとき前に向いていた方*/
 Object& Object::SetPower(std::array<double,4>length, std::array<double,4>angle) {
@@ -74,8 +44,8 @@ Object& Object::SetPower(std::array<double,4>length, std::array<double,4>angle) 
 			.rotatedAt(Robot.center(), Robot_Vec.z)
 			.draw(Palette::Red, Palette::Red, Palette::White, Palette::White);
 		Shape2D::Arrow(Rectframe[i].center().rotatedAt(Robot.center(), Robot_Vec.z)
-			, { Rectframe[i].center().rotatedAt(Robot.center(), Robot_Vec.z).x + Vector[i].x
-			, Rectframe[i].center().rotatedAt(Robot.center(), Robot_Vec.z).y - Vector[i].y }
+			, { Rectframe[i].center().rotatedAt(Robot.center(), Robot_Vec.z).x + Vector[i].rotated(-Robot_Vec.z).x
+			, Rectframe[i].center().rotatedAt(Robot.center(), Robot_Vec.z).y - Vector[i].rotated(-Robot_Vec.z).y }
 			, 6
 			, Vec2{ 8,8 }).draw();
 
@@ -86,90 +56,13 @@ Object& Object::SetPower(std::array<double,4>length, std::array<double,4>angle) 
 			Rectframe[i].rotatedAt(Robot.center(), Robot_Vec.z).drawFrame(0, 3, Palette::Orange);
 		}
 		old_angle[i] = angle[i];
-		/*回転ベクトル以外のベクトルを求める*/
-		Robot_Vec.x += Vector[i].rotatedAt(Robot.center(), Robot_Vec.z).x/100;
-		Robot_Vec.y += Vector[i].rotatedAt(Robot.center(), Robot_Vec.z).y/100;
-		/*出来てない*/
-		Robot_Vec.z += (/*ｘ軸*/length[i] * sin(ToRadians(angle[i]) + (i * -0.5 - 0.75) * Math::Pi) - /*ｙ軸*/length[i] * cos(ToRadians(angle[i]) + (i * -0.5 - 0.25) * Math::Pi))/1000;
-		//Robot_Vec.z += Vec2{ Vector[i].x,Vector[i].y }.rotatedAt(Robot.center(), Robot_Vec.z).rotate((i * -0.5 + 0.25) * Math::Pi).x / 10000;
-		Print << U"::";
-		Print << length[i] * sin(ToRadians(angle[i]) + (i * -0.5 - 0.75) * Math::Pi);
-		Print << length[i] * cos(ToRadians(angle[i]) + (i * -0.5 - 0.25) * Math::Pi);
+
+		int8_t xangle = i == 2 || i == 3 ? -1 : 1;
+		int8_t yangle = i == 1 || i == 2 ? -1 : 1;
+		Robot_Vec.z += (/*ｘ軸*/xangle*(Vector[i].x * cos(0.25 * Math::Pi))
+			+/*ｙ軸*/yangle * (Vector[i].y * cos(-0.25 * Math::Pi))) / 9000;
+		Robot_Vec.x += Vector[i].rotated( -Robot_Vec.z).x / 60;
+		Robot_Vec.y += Vector[i].rotated( -Robot_Vec.z).y / 60;
 	}
-	
 	return *this;
 }
-//Object& Object::SetLF(double length,double angle) {
-//	RectF{ 0,0,LF_frame.w / 3.1,LF_frame.h / 1.5 }.setCenter(LF_frame.center()).rotated(ToRadians(angle))
-//		.draw(Palette::Red, Palette::Red, Palette::White, Palette::White);
-//	Shape2D::Arrow(LF_frame.center()
-//			, { LF_frame.center().x+length * sin(ToRadians(angle))
-//			, LF_frame.center().y - length * cos(ToRadians(angle)) }
-//			, 6
-//			, Vec2{ 8,8 }).draw();
-//
-//	if (fliped_jadge(old_angle[0], angle)) {
-//		time[0] = Time::GetMillisec();
-//	}
-//	if (Time::GetMillisec()-time[0]<200) {
-//		LF_frame.drawFrame(0, 3,Palette::Orange);
-//	}
-//	old_angle[0] = angle;
-//	return *this;
-//}
-//Object& Object::SetRF(double length, double angle) {
-//	RectF{ 0,0,RF_frame.w / 3.1,RF_frame.h / 1.5 }.setCenter(RF_frame.center()).rotated(ToRadians(angle))
-//		.draw(Palette::Red, Palette::Red, Palette::White, Palette::White);
-//	Shape2D::Arrow(RF_frame.center()
-//		, { RF_frame.center().x + length * sin(ToRadians(angle))
-//		, RF_frame.center().y - length * cos(ToRadians(angle)) }
-//		, 6
-//		, Vec2{ 8,8 }).draw();
-//
-//	if (fliped_jadge(old_angle[1], angle)) {
-//		time[1] = Time::GetMillisec();
-//	}
-//	if (Time::GetMillisec() - time[1] < 200) {
-//		RF_frame.drawFrame(0, 3, Palette::Orange);
-//	}
-//	old_angle[1] = angle;
-//	return *this;
-//}
-//Object& Object::SetRB(double length, double angle) {
-//	RectF{ 0,0,RB_frame.w / 3.1,RB_frame.h / 1.5 }.setCenter(RB_frame.center()).rotated(ToRadians(angle))
-//		.draw(Palette::Red, Palette::Red, Palette::White, Palette::White);
-//	Shape2D::Arrow(RB_frame.center()
-//		, { RB_frame.center().x + length * sin(ToRadians(angle))
-//		, RB_frame.center().y - length * cos(ToRadians(angle)) }
-//		, 6
-//		, Vec2{ 8,8 }).draw();
-//
-//	if (fliped_jadge(old_angle[2], angle)) {
-//		time[2] = Time::GetMillisec();
-//	}
-//	if (Time::GetMillisec() - time[2] < 200) {
-//		RB_frame.drawFrame(0, 3, Palette::Orange);
-//	}
-//	old_angle[2] = angle;
-//	return *this;
-//}
-//Object& Object::SetLB(double length, double angle) {
-//	RectF{ 0,0,LB_frame.w / 3.1,LB_frame.h / 1.5 }.setCenter(LB_frame.center()).rotated(ToRadians(angle))
-//		.draw(Palette::Red, Palette::Red, Palette::White, Palette::White);
-//	Shape2D::Arrow(LB_frame.center()
-//			, { LB_frame.center().x + length * sin(ToRadians(angle))
-//			, LB_frame.center().y - length * cos(ToRadians(angle)) }
-//			, 6
-//			, Vec2{ 8,8 }).draw();
-//
-//	if (fliped_jadge(old_angle[3], angle)) {
-//		time[3] = Time::GetMillisec();
-//	}
-//	if (Time::GetMillisec() - time[3] < 200) {
-//		LB_frame.drawFrame(0, 3, Palette::Orange);
-//	}
-//	old_angle[3] = angle;
-//	return *this;
-//}
-
-
